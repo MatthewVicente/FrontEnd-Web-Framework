70dd6f5cfc4f00e14fae24a87a642ccc79baa931
mathewvicente <mathewvicente@gmail.com>
2020-01-20 16:15:35 -0300

    Initiate project and start to build User class

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { User } from './models/User';
+
+const user = new User({ name: 'myname', age: 20 });
+
+user.on('change', () => {
+	console.log("Change #1");
+});
+user.on('click', () => {
+	console.log("Click #1");
+});
+
+user.trigger('change');
