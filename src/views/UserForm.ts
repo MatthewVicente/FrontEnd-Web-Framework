a8eb0532d51e64f743596cb299ef97a9278d0585
mathewvicente <mathewvicente@gmail.com>
2020-01-22 17:01:53 -0300

    Map and bind events to html elements

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,6 +1,16 @@
 export class UserForm {
 	constructor(public parent: Element) {}
 
+	eventsMap(): { [key: string]: () => void } {
+		return {
+			'click:button': this.onButtonClick
+		}
+	}
+
+	onButtonClick(): void {
+		console.log('Hi there');
+	}
+
 	template(): string {
 		return `
 			<div>
@@ -11,9 +21,24 @@ export class UserForm {
 		`
 	}
 
+	bindEvents(fragment: DocumentFragment): void {
+		const eventsMap = this.eventsMap();
+
+		for (let eventkey in eventsMap) {
+			const [eventName, selector] = eventkey.split(':');
+
+			fragment.querySelectorAll(selector).forEach(element => {
+				element.addEventListener(eventName, eventsMap[eventkey]);
+			});
+		}
+
+	}
+
 	render(): void {
 		const templateElement = document.createElement('template');
 		templateElement.innerHTML = this.template();
+
+		this.bindEvents(templateElement.content);
 
 		this.parent.append(templateElement.content);
 	}
