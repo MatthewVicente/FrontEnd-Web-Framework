670695be464171371874c086abfdc2159277ce17
mathewvicente <mathewvicente@gmail.com>
2020-01-23 15:16:41 -0300

    Create method to setRandomAge, and clean html rendered every time it renders

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,7 +1,17 @@
 import { User } from '../models/User';
 
 export class UserForm {
-	constructor(public parent: Element, public model: User) {}
+	constructor(public parent: Element, public model: User) {
+		this.model.on('change', () => {
+			this.bindModel();
+		});
+	}
+
+	bindModel(): void {
+		this.model.on('change', () => {
+			this.render();
+		})
+	}
 
 	// Map events that will be binded to HTML using the structure event_name:element
 	eventsMap(): { [key: string]: () => void } {
@@ -10,8 +20,8 @@ export class UserForm {
 		}
 	}
 
-	onSetAgeClick(): void {
-		console.log("button was clicked");
+	onSetAgeClick = (): void => {
+		this.model.setRandomAge();
 	}
 
 	// Generate html that will be renderer
@@ -44,6 +54,8 @@ export class UserForm {
 
 	// Render template and run bindEvents()
 	render(): void {
+		this.parent.innerHTML = '';
+
 		const templateElement = document.createElement('template');
 		templateElement.innerHTML = this.template();
 
