bee8f39f5f93f8e0f3d42246f81a3996d5c0821b
mathewvicente <mathewvicente@gmail.com>
2020-01-23 15:49:43 -0300

    Create onSetNameClick method

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -16,8 +16,20 @@ export class UserForm {
 	// Map events that will be binded to HTML using the structure event_name:element
 	eventsMap(): { [key: string]: () => void } {
 		return {
-			'click:#set-age': this.onSetAgeClick
+			'click:#set-age': this.onSetAgeClick,
+			'click:#set-name': this.onSetNameClick
 		}
+	}
+
+	onSetNameClick = (): void => {
+		const input = this.parent.querySelector('input');
+
+		if (input) {
+			const name = input.value;
+
+			this.model.set({ name });
+		}
+
 	}
 
 	onSetAgeClick = (): void => {
@@ -32,7 +44,7 @@ export class UserForm {
 				<div>User name: ${this.model.get('name')}</div>
 				<div>User age: ${this.model.get('age')}</div>
 				<input />
-				<button>Click Me</button>
+				<button id="set-name">Change Name</button>
 				<button id="set-age">Set Random Age</button>
 			</div>
 		`
